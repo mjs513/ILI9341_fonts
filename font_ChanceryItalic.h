c48abc92252bd179fa4d4bcb652141cda65d388f
Mike S <CyberMerln@gmail.com>
2020-01-11 10:42:12 -0500

    Updated with chancery and font header

---
+++ font_ChanceryItalic.h
@@ -0,0 +1,43 @@
+#ifndef _ILI9341_t3_font_ChanceryItalic_
+#define _ILI9341_t3_font_ChanceryItalic_
+
+#if __has_include(<RA8875.h>)
+	#include "RA8875.h"
+#elif __has_include(<ILI9488.h>)
+	#include "ili9488.h"
+#elif __has_include(<ILI9341_t3n.h>)
+	#include "ILI9341_t3n.h"
+#elif __has_include(<ILI9341.h>)
+	#include "ili9341.h"
+#elif __has_include(<ST7735_t3.h>)
+	#include "ST7735_t3.h"
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const ILI9341_t3_font_t Chancery_8_Italic;
+extern const ILI9341_t3_font_t Chancery_9_Italic;
+extern const ILI9341_t3_font_t Chancery_10_Italic;
+extern const ILI9341_t3_font_t Chancery_11_Italic;
+extern const ILI9341_t3_font_t Chancery_12_Italic;
+extern const ILI9341_t3_font_t Chancery_13_Italic;
+extern const ILI9341_t3_font_t Chancery_14_Italic;
+extern const ILI9341_t3_font_t Chancery_16_Italic;
+extern const ILI9341_t3_font_t Chancery_18_Italic;
+extern const ILI9341_t3_font_t Chancery_20_Italic;
+extern const ILI9341_t3_font_t Chancery_24_Italic;
+extern const ILI9341_t3_font_t Chancery_28_Italic;
+extern const ILI9341_t3_font_t Chancery_32_Italic;
+extern const ILI9341_t3_font_t Chancery_40_Italic;
+extern const ILI9341_t3_font_t Chancery_48_Italic;
+extern const ILI9341_t3_font_t Chancery_60_Italic;
+extern const ILI9341_t3_font_t Chancery_72_Italic;
+extern const ILI9341_t3_font_t Chancery_96_Italic;
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif
